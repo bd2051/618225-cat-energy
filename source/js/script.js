166617ad8a189218ffbeccc0e25c05284b8da81e
Дмитрий <bd2051@mail.ru>
2018-05-20 22:22:24 +0300

    исправлено название блока js-hide

--- source/js/script.js
+++ source/js/script.js
@@ -3,15 +3,15 @@
   var popup = document.querySelector(".site-list");
   var marker = document.querySelector(".map__marker");
 
-  popup.classList.add("js__hide")
-  marker.classList.add("js__hide--marker")
+  popup.classList.add("js-hide")
+  marker.classList.add("js-hide--marker")
   menu.classList.remove("main-nav--opened")
   menu.classList.add("main-nav--closed")
-  btn.classList.remove("js__hide")
+  btn.classList.remove("js-hide")
 
   btn.addEventListener("click", function (evt) {
     evt.preventDefault();
-    popup.classList.toggle("js__hide");
+    popup.classList.toggle("js-hide");
     menu.classList.toggle("main-nav--opened")
     menu.classList.toggle("main-nav--closed")
   });
